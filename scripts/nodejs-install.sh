29a8c10d5827f8045f4511dc672312a1d691595b
Daniyal <154412066+mdaniyal05@users.noreply.github.com>
2025-10-11 00:15:29 +0500

    Refactor nodejs script

--- scripts/nodejs-install.sh
+++ scripts/nodejs-install.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-echo "Installing node.js via nvm...."
+set -e
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-\. "$HOME/.nvm/nvm.sh"
-nvm install 22
+if ! command -v nvm &> /dev/null; then
+  echo "Installing NVM...."
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+  \. "$HOME/.nvm/nvm.sh"
+else
+  echo "NVM is already installed"
+fi
+
+echo "Installing Node.js LTS via NVM...."
+
+nvm install --lts
+
+echo "Node.js installed successfully."
+
+node --version
+npm --version
+
+echo "List all installed Node.js versions with 'nvm ls'"
